77cc9e0721e707b669d317c6712052b7bd897e00
Ms-KL <thekristyleigh@gmail.com>
2023-03-25 10:19:18 +0800

    Admin setup for mentor

--- mentorschedulingtool/mentors/admin.py
+++ mentorschedulingtool/mentors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Mentor
 
-# Register your models here.
+# # Register your models here.
+
+# -----------------------
+# ADMIN BLOCK
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name','last_name', 'email','city', 'is_active', 'date_created')
+
+admin.site.register(Mentor)
