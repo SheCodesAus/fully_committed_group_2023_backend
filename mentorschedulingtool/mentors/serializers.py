6d28ae8669a307e2078dd6ecc6a7d03c6688c7f7
Ms-KL <thekristyleigh@gmail.com>
2023-03-28 00:47:36 +0800

    Add detailed session view on mentor data

--- mentorschedulingtool/mentors/serializers.py
+++ mentorschedulingtool/mentors/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
 
-
-from .models import Mentor
+from .models import Mentor,Session
 
 # created for the session view - to avoid session repetition
 class MentorWithoutSessionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentor
         exclude = ('sessions', )
+
+# created to get full session view with session details
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = '__all__'
 class MentorSerializer(serializers.ModelSerializer):
+    sessions = SessionSerializer(many=True)
 
     class Meta:
         model = Mentor
