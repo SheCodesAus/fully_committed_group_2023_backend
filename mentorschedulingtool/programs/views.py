4c9fe6ba1cdbb1b00c60ea5e74ae5636acf33f22
Ms-KL <thekristyleigh@gmail.com>
2023-04-01 20:03:02 +0800

    Add superuser status to restrict lead mentors,

--- mentorschedulingtool/programs/views.py
+++ mentorschedulingtool/programs/views.py
@@ -1,14 +1,17 @@
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Program
 from .serializers import ProgramSerializer
+from mentors.permissions import IsSuperUserOrReadOnly
 # Create your views here.
 
 class ProgramList (generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsSuperUserOrReadOnly]
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
 
 class ProgramListDetail (generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsSuperUserOrReadOnly]
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
