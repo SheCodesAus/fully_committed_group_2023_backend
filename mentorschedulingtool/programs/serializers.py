0142bbae6465c03929f3e3b94d6e6543df0c1a1d
Ms-KL <thekristyleigh@gmail.com>
2023-04-01 08:17:16 +0800

    auto-calc sums of mentors required and assigned in programs

--- mentorschedulingtool/programs/serializers.py
+++ mentorschedulingtool/programs/serializers.py
@@ -9,15 +9,20 @@ from sess.models import Session
 # i can simplify JSON file to just id of sessions if required. Let me know
 class ProgramSerializer(serializers.ModelSerializer):
     sessions = SessionSerializer(many=True, read_only=True)
+    mentors_required = serializers.ReadOnlyField()
+    mentors_assigned = serializers.ReadOnlyField()
     class Meta:
         model = Program
         fields = [
             'id',
             'program_name',
+            'mentors_required',
+            'mentors_assigned',
             'start_date',
             'end_date',
             'city',
             'program_type',
             'sessions'
+
         ]
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'mentors_required', 'mentors_assigned']
