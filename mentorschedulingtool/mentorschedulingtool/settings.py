252c560bb2bc329eb67ff94deb7c1625991c6ddc
Ms-KL <thekristyleigh@gmail.com>
2023-04-02 17:24:26 +0800

    session auth for token

--- mentorschedulingtool/mentorschedulingtool/settings.py
+++ mentorschedulingtool/mentorschedulingtool/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
