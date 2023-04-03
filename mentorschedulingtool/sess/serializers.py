10658f963a9f2b0b29231e511d2e76e730817790
Ms-KL <thekristyleigh@gmail.com>
2023-04-03 08:42:31 +0800

    Split session serilializer into list and detail for clarity and field control

--- mentorschedulingtool/sess/serializers.py
+++ mentorschedulingtool/sess/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from mentors.serializers import MentorSerializer
+from mentors.serializers import MentorSerializer, MentorDetailSerializer
 
 
 from .models import Session
 from mentors.models import Mentor
 
-
+# /sessions/
 class SessionSerializer(serializers.ModelSerializer):
     mentors = MentorSerializer(many=True, read_only=True)
     mentors_assigned = serializers.ReadOnlyField()
@@ -15,3 +15,12 @@ class SessionSerializer(serializers.ModelSerializer):
         fields = ['id', 'session_name', 'mentors_required', 'mentors_assigned', 'date', 'city', 'module_type', 'program', 'mentors']
         read_only_fields = ['id']
 
+# /sessions/<id:pk>/
+class SessionDetailSerializer(SessionSerializer):
+    mentors = MentorDetailSerializer(many=True, read_only=True)
+    mentors_assigned = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Session
+        fields = ['id', 'session_name', 'mentors_required', 'mentors_assigned', 'date', 'city', 'module_type', 'program', 'mentors']
+        read_only_fields = ['id']
